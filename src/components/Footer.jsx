96c8bbb14da7c7709337264defbf6baaef1cb7b3
Michael Chung <michael061195@gmail.com>
2024-09-04 17:06:42 -0500

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <small>&copy; Copyright by Magikmaik. I copied this so you can too</small>
-      <small>last checked today</small>
+      <small>&copy; Copyright by Maik.</small>
     </footer>
   );
 }
